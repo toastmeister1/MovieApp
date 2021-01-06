adb22e963e710c841431975b0b7bb2f259758bf6
Joseph <wjsdudtkd95@gmail.com>
2021-01-06 22:01:13 +0900

    [feat] BaseActivity 적용

---
+++ app/src/main/java/com/joseph/tmdb_mvvm/BaseActivity.kt
@@ -0,0 +1,13 @@
+package com.joseph.tmdb_mvvm
+
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    inline fun <reified T: ViewDataBinding> binding(
+        @LayoutRes resId: Int
+    ): Lazy<T> = lazy { DataBindingUtil.setContentView(this, resId) }
+}
--- app/src/main/java/com/joseph/tmdb_mvvm/MainActivity.kt
+++ app/src/main/java/com/joseph/tmdb_mvvm/MainActivity.kt
@@ -2,10 +2,16 @@ package com.joseph.tmdb_mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.joseph.tmdb_mvvm.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : BaseActivity() {
+
+    private val binding: ActivityMainBinding by binding(R.layout.activity_main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
     }
 }
