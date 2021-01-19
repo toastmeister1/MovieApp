00919e06045a63ec4b39a4c2d520334e44ac89eb
Joseph <wjsdudtkd95@gmail.com>
2021-01-19 17:00:08 +0900

    [feat] MovieDetail 구현
    - 데이터 바인딩 작업중
    - ChipGroup을 바인딩 해서 Chip 구현 완료
    - XML 일부 수정
    - BindingAdapter의 이미지 로딩 메소드를 Thumb과 Origin으로 분리

--- app/src/main/java/com/joseph/tmdb_mvvm/util/BindingAdapter.kt
+++ app/src/main/java/com/joseph/tmdb_mvvm/util/BindingAdapter.kt
@@ -1,12 +1,16 @@
 package com.joseph.tmdb_mvvm.util
 
+import android.view.Gravity
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 import com.joseph.tmdb_mvvm.R
+import com.joseph.tmdb_mvvm.model.MovieDetail
 import com.joseph.tmdb_mvvm.ui.frag.home.adapter.MovieListAdapter
 
 const val IMAGE_URL_THUMB = "https://image.tmdb.org/t/p/w500"
@@ -19,8 +23,19 @@ fun setImageWithSrc(view: ImageView, src: Int) {
             .into(view)
 }
 
-@BindingAdapter("setImageWithURL")
-fun setImageWithUrl(view: ImageView, url: String?) {
+@BindingAdapter("setThumbImageWithURL")
+fun setThumbImageWithUrl(view: ImageView, url: String?) {
+    if (url != null) {
+        Glide.with(view.context)
+                .load(IMAGE_URL_THUMB + url)
+                .override(320, 480)
+                .placeholder(R.drawable.image_placeholder)
+                .into(view)
+    }
+}
+
+@BindingAdapter("setOriginImageWithURL")
+fun setOriginImageWithUrl(view: ImageView, url: String?) {
     if (url != null) {
         Glide.with(view.context)
                 .load(IMAGE_URL_ORIGIN + url)
@@ -28,6 +43,7 @@ fun setImageWithUrl(view: ImageView, url: String?) {
                 .into(view)
     }
 }
+
 
 @BindingAdapter("initHorizonRecyclerAdapter")
 fun initHorizontalRecyclerAdapter(view: RecyclerView, adapter: MovieListAdapter) {
@@ -46,10 +62,30 @@ fun <T : Any> setText(view: TextView, text: T?) {
     }
 }
 
-@BindingAdapter("setTextList")
-fun <T : Any> setTextList(view: TextView, textList: List<T>) {
+@BindingAdapter("bindList")
+fun bindList(view: ChipGroup, list: List<MovieDetail.Genre>?) {
+
+    list?.forEach { genre ->
+        val chip = Chip(view.context).apply{
+            text = genre.name
+            isCheckable = false
+            setTextAppearance(R.style.ChipTextAppearance)
+            setChipBackgroundColorResource(R.color.watchaPink)
+            elevation = 2F
+            gravity = Gravity.CENTER
+        }
+
+        view.addView(chip)
+    }
 }
 
-
+@BindingAdapter("isYouthCanWatch")
+fun isYouthCanWatch (view: TextView, isAdult: Boolean) {
+    if(isAdult) {
+        view.text = "관람불가"
+    } else {
+        view.text = "관람가능"
+    }
+}
 
 
